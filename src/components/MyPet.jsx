6de34eb89cdd04b1ecafe1ab065ca78bf6f531c3
Tenuka <tenukaperera1025@gmail.com>
2026-08-10 22:52:51 +0530

    added main face section

---
+++ src/components/MyPet.jsx
@@ -0,0 +1,13 @@
+function MyPet(){
+
+    return(
+        <svg width={200} height={200}>
+            <rect rx={5} x={24} y={33} width={70} height={24} stroke="black" fill="#2196F3" stroke-width ="0.4"></rect>
+            <rect rx={11} x={10} y={50} width={100} height={22} stroke="black" fill="#30AFFF" stroke-width ="0.4"></rect>
+            <rect rx={14} x={15} y={66} width={90} height={70} stroke="black" fill="#E3F2FD" ></rect>
+            <rect rx={14} x={25} y={75} width={70} height={50} stroke="#E3F2FD" fill="black" stroke-width ="0.7" ></rect>
+            
+        </svg>
+    )
+}
+export default MyPet
