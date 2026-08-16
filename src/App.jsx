90878bf0c9e9e436617d566d1f151245129de873
Tenuka <tenukaperera1025@gmail.com>
2026-08-16 22:16:19 +0530

    added animation images and metpet component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css"
 
 function App(){
     return(
-        <div>
+        <div style={{background:"transparent"}}>
             <MyPet/>
         </div>
     )
--- src/components/MyPet.jsx
+++ src/components/MyPet.jsx
@@ -1,26 +1,24 @@
-function MyPet(){
+import { useEffect, useState } from "react";
+import eye01 from '../assets/Robot/Eye/eye01.png'
+import eye02 from '../assets/Robot/Eye/eye02.png'
+
+const images = [eye01,eye02]
+export default function AutoImageSwitcher(){
+
+    const [currentIndex , setCurrentIndex] = useState(0);
+
+
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            setCurrentIndex((prev)=>(prev === images.length -1 ? 0: prev+1));
+        }, 1000);
+
+        return()=> clearInterval(interval);
+    },[]);
 
     return(
-        <svg width={200} height={200}>
-            {/* Coffee Hat*/}
-            <rect rx={5} x={24} y={33} width={70} height={24} stroke="black" fill="#2196F3" stroke-width ="0.4"></rect>
-            <rect rx={11} x={10} y={50} width={100} height={22} stroke="black" fill="#30AFFF" stroke-width ="0.4"></rect>
-
-            {/* Head*/}
-            <rect rx={14} x={15} y={66} width={90} height={70} stroke="black" fill="#E3F2FD" ></rect>
-            <rect rx={14} x={25} y={75} width={70} height={50} stroke="#E3F2FD" fill="black" stroke-width ="0.7" ></rect>
-
-            {/* Eye Brows*/}
-              <path d="M 80 15 C 70 20, 110 20, 90 15" stroke="black" fill="transparent" stroke-width ="3" />
-
-            {/* Eyes*/}
-            <ellipse cx={120} cy={130} rx={6.5} ry={5} transform="rotate(90,100,75)" fill="#5DF8D8"></ellipse>
-            <ellipse cx={120} cy={100} rx={6.5} ry={5} transform="rotate(90,100,75)" fill="#5DF8D8"></ellipse>
-            
-            
-
-            
-        </svg>
+        <div style={{background:"transparent"}}>
+            <img src={images[currentIndex]} alt="Robot Eye movement"/>
+        </div>
     )
 }
-export default MyPet
