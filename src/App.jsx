25293e13bbd55d279aca82d51345ee7e4edcad81
Tenuka <tenukaperera1025@gmail.com>
2026-08-18 20:09:10 +0530

    added sleeping frames and made pet moveable

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,22 @@ import "./App.css"
 
 function App(){
     return(
-        <div data-tauri-drag-region style={{background:"transparent",width:"100vw",
-    height:"100vh",}}>
-            <MyPet/>
-        </div>
+        <>
+            <div 
+                data-tauri-drag-region 
+                style={{
+                    background:"transparent",
+                    width:"100vw",
+                    height:"100vh",
+                    position:"fixed",
+                    top:0,
+                    left:0,
+                }}
+            />
+            <div style={{background:"transparent"}}>
+                <MyPet/>
+            </div>
+        </>
     )
 }
 export default App
--- src/components/MyPet.jsx
+++ src/components/MyPet.jsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 import eye01 from '../assets/Robot/Eye/eye01.png'
 import eye02 from '../assets/Robot/Eye/eye02.png'
+ 
 
-const images = [eye01,eye02]
+const onboot_images = [eye01,eye02]
+const sleeping = import.meta.glob('../assets/Robot/Sleeping/*.png',{eager : true})
+
 export default function AutoImageSwitcher(){
-
+    const sleepingframes = Object.values(sleeping).map(m => m.default)
     const [currentIndex , setCurrentIndex] = useState(0);
-
+    
 
     useEffect(()=>{
         const interval = setInterval(()=>{
-            setCurrentIndex((prev)=>(prev === images.length -1 ? 0: prev+1));
+            setCurrentIndex((prev)=>(prev === onboot_images.length -1 ? 0: prev+1));
         }, 1000);
 
         return()=> clearInterval(interval);
@@ -18,7 +21,7 @@ export default function AutoImageSwitcher(){
 
     return(
         <div style={{background:"transparent"}}>
-            <img src={images[currentIndex]} alt="Robot Eye movement" draggable="false"/>
+            <img src={onboot_images[currentIndex]} alt="Robot Eye movement" draggable="false"/>
         </div>
     )
 }
